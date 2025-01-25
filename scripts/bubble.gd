cee87a99c78df7dadb93c9e655734302bcb8ba3c
Sandra Moen <sandramoen01@gmail.com>
2025-01-25 09:28:26 +0100

    changed to portrait, player may now enter and leave bubbles

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -1,9 +1,13 @@
 class_name Bubble
-extends Node2D
+extends Area2D
 
-@export var _rotation_speed := 2.0
+@export var _rotation_speed := 3.0
 
 var _player: Player
+
+
+func _ready():
+	body_entered.connect(_on_area_entered)
 
 
 func _process(delta: float) -> void:
@@ -12,9 +16,23 @@ func _process(delta: float) -> void:
 		_player.rotation = rotation
 
 
-func enter_player(player) -> void:
+func enter_player(player: Player) -> void:
 	_player = player
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_SPRING)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(player, "position", position, 0.25)
 
 
 func exit_player() -> void:
 	_player = null
+
+
+func _on_area_entered(area_that_entered: Node) -> void:
+	if area_that_entered is Player:
+		print("Player entered the bubble!")
+		var player = area_that_entered
+		
+		enter_player(player)
+		player.bubble = self
+		player.velocity = Vector2.ZERO
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@export var shoot_speed = 600
+@export var shoot_speed = 1000
 
 var first_player_event:= true
 
@@ -26,19 +26,21 @@ func _move_player() -> void:
 
 
 func enter_player_in_first_bubble():
-	var bubble = $entities/Bubbles/FirstBubble
+	var bubble = $entities/Bubbles/FirstBubble as Bubble
+	player.bubble = bubble
+	bubble.enter_player(player)
 	
 	var tween := create_tween()
 	tween.set_trans(Tween.TRANS_EXPO)
 	tween.set_ease(Tween.EASE_OUT)
 	tween.tween_property(player, "position", bubble.position, 1.0)
-	tween.finished.connect(func() -> void:
-		bubble.enter_player(player)
-		player.bubble = bubble
-	)
 
 
-func shoot_player() -> void:	
+func shoot_player() -> void:
+	if player.bubble == null:
+		print("player.bubble is null")
+		return
+	
 	var shoot_direction = Vector2.UP.rotated(player.bubble.rotation)
 	player.bubble.exit_player()
 	player.bubble = null
